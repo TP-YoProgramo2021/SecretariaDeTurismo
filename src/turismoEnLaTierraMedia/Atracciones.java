41d766d6df9c9b353182dbf8b3d82cdc51c56be4
cristhianvega <38482385+cristhianvega@users.noreply.github.com>
2021-08-27 10:11:16 -0300

    Update Atracciones.java

--- src/turismoEnLaTierraMedia/Atracciones.java
+++ src/turismoEnLaTierraMedia/Atracciones.java
@@ -1,30 +1,6 @@
 package turismoEnLaTierraMedia;
 
-public class Atracciones {
+public class Atraccion {
 
-
-	private double costo;
-	private double tiempo;
-	private int cupo;
-	private String tipo;
-
-	public Atracciones(double costo, double tiempo, int cupo, String tipo) {
-		this.costo = costo;
-		this.tiempo = tiempo;
-		this.cupo = cupo;
-		this.tipo = tipo;
-	}
-
-	public double getCosto() {
-		return costo;
-	}
-
-	public double getTiempo() {
-		return tiempo;
-	}
-
-	public String getTipo() {
-		return tipo;
-	}
 	
 }
