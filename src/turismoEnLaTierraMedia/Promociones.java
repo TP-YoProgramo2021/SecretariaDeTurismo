2a783827f52b194dfb39d0a205b374b595a04bab
cristhianvega <38482385+cristhianvega@users.noreply.github.com>
2021-08-27 10:12:33 -0300

    Update Promociones.java

--- src/turismoEnLaTierraMedia/Promociones.java
+++ src/turismoEnLaTierraMedia/Promociones.java
@@ -1,21 +1,5 @@
 package turismoEnLaTierraMedia;
 
-public class Promociones {
-	private double costoPaquete;
-	private double tiempoTotal;
-	private String tipoDeAtraccion;
+public class Promocion {
 	
-	public Promociones(double costoPaquete, double tiempoTotal, String tipoDeAtraccion) {
-		this.costoPaquete = costoPaquete;
-		this.tiempoTotal = tiempoTotal;
-		this.tipoDeAtraccion = tipoDeAtraccion;
-	}
-
-	public double getCostoPaquete() {
-		return costoPaquete;
-	}
-
-	public double getTiempoTotal() {
-		return tiempoTotal;
-	}
 }
